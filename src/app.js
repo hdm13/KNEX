18676b6b8eb71a6d2c25885f97d7a53ed407af01
hdm13 <heath.d.mcgraw@gmail.com>
2022-09-09 14:40:20 -0400

    2nd edit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8081;
+const knex = require('knex')(require('../knexfile.js'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Application up and running.')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Pets listening on port ${port}`)
+})
+
+app.get('/pets', (req, res) => {
+    knex('pet')
+    .SELECT('*')
+    .then(data => {
+        let petNames = data.map(pet => pet.name)
+    })
 })
